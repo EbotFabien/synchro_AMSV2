cda7cb654718089eeb5f227bc95fc997593f15ac
fabienclassic <fabienclassic@gmail.com>
2023-06-15 10:12:39 +0100

    done

--- project/app/entity/biblio/routes.py
+++ project/app/entity/biblio/routes.py
@@ -2,19 +2,25 @@ from flask import render_template, url_for,flash,redirect,request,abort,Blueprin
 from app import db4,create_app,db2
 from flask_cors import CORS,cross_origin
 
+#db participant
 agent_sec = db4.collection('Extension')
 
+voie_da = db4.collection('voie')
+
+#db logement
 type_log = db2.collection('type_log')
 
 extension = db2.collection('extenssion')
 
 voie2 = db2.collection('voie')
 
-voie_da = db4.collection('voie')
-
 db_piece = db2.collection('piece')
 
 db_rubriq = db2.collection('rubriq')
+
+db_cles = db2.collection('cles')
+
+db_compteur = db2.collection('compteur')
 
 
 
@@ -54,6 +60,29 @@ def creatpiece_log():
     return jsonify({"stat":stat1}), 200
 
 @cross_origin(origin=["http://127.0.0.1:5274","http://195.15.228.250","*"],headers=['Content-Type','Authorization'],automatic_options=False)
+@biblio.route('/compteur/ajouter/', methods=['POST'])
+def creatcompteur_log():
+    id=request.json['id']
+    todo = db_compteur.document(id).get()
+    stat1=0
+    if  todo.to_dict() is None :
+        db_compteur.document(id).set(request.json)
+        stat1=1
+    return jsonify({"stat":stat1}), 200
+
+@cross_origin(origin=["http://127.0.0.1:5274","http://195.15.228.250","*"],headers=['Content-Type','Authorization'],automatic_options=False)
+@biblio.route('/compteur/modify/', methods=['POST'])
+def editcompteur():
+    id=request.json['id']
+    todo = db_compteur.document(id).get()
+    stat1=0
+    if  todo.to_dict():
+        db_compteur.document(id).update(request.json)
+        stat1=1
+    return jsonify({"stat":stat1}), 200
+
+
+@cross_origin(origin=["http://127.0.0.1:5274","http://195.15.228.250","*"],headers=['Content-Type','Authorization'],automatic_options=False)
 @biblio.route('/rubric/modify/', methods=['POST'])
 def editrubric():
     id=request.json['id']
@@ -74,6 +103,29 @@ def creatrubric_log():
         db_rubriq.document(id).set(request.json)
         stat1=1
     return jsonify({"stat":stat1}), 200
+
+@cross_origin(origin=["http://127.0.0.1:5274","http://195.15.228.250","*"],headers=['Content-Type','Authorization'],automatic_options=False)
+@biblio.route('/clef/ajouter/', methods=['POST'])
+def creatclef_log():
+    id=request.json['id']
+    todo = db_cles.document(id).get()
+    stat1=0
+    if  todo.to_dict() is None :
+        db_cles.document(id).set(request.json)
+        stat1=1
+    return jsonify({"stat":stat1}), 200
+
+@cross_origin(origin=["http://127.0.0.1:5274","http://195.15.228.250","*"],headers=['Content-Type','Authorization'],automatic_options=False)
+@biblio.route('/clef/modify/', methods=['POST'])
+def editclef():
+    id=request.json['id']
+    todo = db_cles.document(id).get()
+    stat1=0
+    if  todo.to_dict():
+        db_cles.document(id).update(request.json)
+        stat1=1
+    return jsonify({"stat":stat1}), 200
+
 
 @cross_origin(origin=["http://127.0.0.1:5274","http://195.15.228.250","*"],headers=['Content-Type','Authorization'],automatic_options=False)
 @biblio.route('/type_log/modify/', methods=['POST'])
